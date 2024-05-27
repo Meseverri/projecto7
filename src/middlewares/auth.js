dd440bbb0288ca5821e80a008470517536d25141
Meseverri <meseverrimarquez5@gmail.com>
2024-05-27 20:05:35 +0200

    authentication middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
+const { verifyJwt } = require('../config/jwt');
+const { User } = require('../api/modelos/users');
+
 require('dotenv').config();
 
 const authenticateUser = (req, res, next) => {
   const token = req.header('Authorization').replace('Bearer ', '');
-
   if (!token) {
     return res.status(401).send({ error: 'Access denied. No token provided.' });
   }
@@ -17,15 +19,39 @@ const authenticateUser = (req, res, next) => {
   }
 };
 
-const authorizeRoles = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).send({ error: 'Access denied. You do not have the required role.' });
+const adminAuth = async (req, res, next) => {
+    const token =req.headers.authorization;
+    if (!token) {
+      return res.status(401).send({ error: 'Access denied. No token provided.' });
     }
+    const parsedToken = token.replace("Bearer ", "");
+    const { id } = verifyJwt(parsedToken);
+    
+    const user = await User.findById(id);
+
+
+    if (user.role!=="admin") {
+      return res.status(403).send({ error: 'Access denied. You do not have the required role.' });
+    };
     next();
   };
-};
+
+  const authorAuth = async (req, res, next) => {
+    const token =req.headers.authorization;
+    if (!token) {
+      return res.status(401).send({ error: 'Access denied. No token provided.' });
+    }
+    const parsedToken = token.replace("Bearer ", "");
+    const { id } = verifyJwt(parsedToken);
+    
+    const user = await User.findById(id);
 
 
+    if (user.role!=="author") {
+      return res.status(403).send({ error: 'Access denied. You do not have the required role.' });
+    };
+    next();
+  };
 
-module.exports = { authenticateUser, authorizeRoles };
+
+module.exports = { authenticateUser, adminAuth,authorAuth };
