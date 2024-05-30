3b957e0633b6985e0af6f9b48fc417e06286ffe4
Meseverri <meseverrimarquez5@gmail.com>
2024-05-30 14:32:17 +0200

    ids standarization

--- src/api/rutas/users.js
+++ src/api/rutas/users.js
@@ -6,7 +6,7 @@ const usersRoutes = require("express").Router();
 
 usersRoutes.post("/register", register);
 usersRoutes.post("/login", login);
-usersRoutes.get("/:id", getUser);
+usersRoutes.get("/:userId", getUser);
 usersRoutes.get("", getUsers);
 usersRoutes.delete("/:userId", authenticateUser,deleteUser);
 
