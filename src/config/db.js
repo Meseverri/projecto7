548e365aacb6bcfce7f092304387161e4c38e133
Meseverri <meseverrimarquez5@gmail.com>
2024-05-31 16:30:09 +0200

    Remove console log of ASCII art on successful database connection.

--- src/config/db.js
+++ src/config/db.js
@@ -4,16 +4,7 @@ const connectDb=async ()=>{
     try {
         await mongoose.connect(process.env.DB_url_production);
         console.log("connected to db")
-        console.log(`
-    **     **  ******   **      **  ******   ******
-    ***   *** **    **  ***     ** **    ** **    **
-    **** ****/**      * ****    **/**      /**      *
-    ** *** **/**      * ** **   **/**      /**      *
-    **     **/**      * **  **  **/** *****/**      *
-    **     ** **    **  **   ** **/**    ** **    **
-    **     **  ******   **    ****  *******  ****** 
-`)
-
+        
     } catch (error) {
         console.log("Error" +error)
     }
